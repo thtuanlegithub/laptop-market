d3137ad827314f7fd77e59714229795863f9a0d7
tuan <thanhtuanse16@gmail.com>
2023-06-01 19:16:31 +0700

    add Search Activity

---
+++ app/src/main/java/com/example/laptop_market/activity/SearchActivity.java
@@ -0,0 +1,16 @@
+package com.example.laptop_market.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.laptop_market.R;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
