c1ad91af019bbf81961c9de76d6bc39ffd3e9c4a
tuan <thanhtuanse16@gmail.com>
2023-07-02 16:26:54 +0700

    fix bug

--- app/src/main/java/com/example/laptop_market/view/adapters/PostActiveAdapter.java
+++ app/src/main/java/com/example/laptop_market/view/adapters/PostActiveAdapter.java
@@ -33,10 +33,10 @@ public class PostActiveAdapter extends RecyclerView.Adapter<PostActiveAdapter.Po
         if(postActive==null){
             return;
         }
-        holder.imgPostActive.setImageResource(postActive.getImg());
-        holder.titlePostActive.setText(postActive.getLaptopName());
-        holder.pricePostActive.setText(postActive.getPrice());
-        holder.addressPostActive.setText(postActive.getAddress());
+//        holder.imgPostActive.setImageResource(postActive.getPostMainImage());
+//        holder.titlePostActive.setText(postActive.getSellerName());
+//        holder.pricePostActive.setText(postActive.geSe);
+//        holder.addressPostActive.setText(postActive.getAddress());
     }
 
     @Override
